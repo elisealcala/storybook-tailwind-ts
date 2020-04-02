c68ea820790cc9cd96886d14d769f04678e8e64e
Elizabeth Alcalá <elizabethalcala28@gmail.com>
2020-04-01 20:06:59 -0500

    add Layout component to storybook

---
+++ .storybook/Layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../src/scss/tailwind.scss';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="px-20 py-10">
+      {children}
+    </div>
+  )
+}
+
+export default Layout;
